909310426c85309aa3d376d13015aaf88f5ca30a
Zülal Özçelik <zllzclk17@gmail.com>
2024-10-31 15:46:28 +0300

    Add project files.

---
+++ project8_RapidApiCurrency/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace project8_RapidApiCurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDollar = new System.Windows.Forms.Label();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdbDollar = new System.Windows.Forms.RadioButton();
+            this.rdbEuro = new System.Windows.Forms.RadioButton();
+            this.rdbPound = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTotalPrice = new System.Windows.Forms.TextBox();
+            this.LblPound = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDollar
+            // 
+            this.lblDollar.AutoSize = true;
+            this.lblDollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDollar.Location = new System.Drawing.Point(103, 78);
+            this.lblDollar.Name = "lblDollar";
+            this.lblDollar.Size = new System.Drawing.Size(64, 25);
+            this.lblDollar.TabIndex = 0;
+            this.lblDollar.Text = "label1";
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEuro.Location = new System.Drawing.Point(315, 78);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(64, 25);
+            this.lblEuro.TabIndex = 2;
+            this.lblEuro.Text = "label3";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(352, 208);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(197, 22);
+            this.txtUnitPrice.TabIndex = 4;
+            this.txtUnitPrice.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(226, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Birim Tutar:";
+            // 
+            // rdbDollar
+            // 
+            this.rdbDollar.AutoSize = true;
+            this.rdbDollar.Location = new System.Drawing.Point(118, 128);
+            this.rdbDollar.Name = "rdbDollar";
+            this.rdbDollar.Size = new System.Drawing.Size(61, 20);
+            this.rdbDollar.TabIndex = 6;
+            this.rdbDollar.TabStop = true;
+            this.rdbDollar.Text = "Dolar";
+            this.rdbDollar.UseVisualStyleBackColor = true;
+            this.rdbDollar.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdbEuro
+            // 
+            this.rdbEuro.AutoSize = true;
+            this.rdbEuro.Location = new System.Drawing.Point(320, 128);
+            this.rdbEuro.Name = "rdbEuro";
+            this.rdbEuro.Size = new System.Drawing.Size(56, 20);
+            this.rdbEuro.TabIndex = 7;
+            this.rdbEuro.TabStop = true;
+            this.rdbEuro.Text = "Euro";
+            this.rdbEuro.UseVisualStyleBackColor = true;
+            // 
+            // rdbPound
+            // 
+            this.rdbPound.AutoSize = true;
+            this.rdbPound.Location = new System.Drawing.Point(532, 128);
+            this.rdbPound.Name = "rdbPound";
+            this.rdbPound.Size = new System.Drawing.Size(65, 20);
+            this.rdbPound.TabIndex = 8;
+            this.rdbPound.TabStop = true;
+            this.rdbPound.Text = "Sterlin";
+            this.rdbPound.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(352, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 43);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "İşlemi Yap";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(182, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Ödenecek Tutar:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTotalPrice
+            // 
+            this.txtTotalPrice.Location = new System.Drawing.Point(352, 240);
+            this.txtTotalPrice.Name = "txtTotalPrice";
+            this.txtTotalPrice.Size = new System.Drawing.Size(197, 22);
+            this.txtTotalPrice.TabIndex = 10;
+            // 
+            // LblPound
+            // 
+            this.LblPound.AutoSize = true;
+            this.LblPound.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblPound.Location = new System.Drawing.Point(533, 78);
+            this.LblPound.Name = "LblPound";
+            this.LblPound.Size = new System.Drawing.Size(64, 25);
+            this.LblPound.TabIndex = 12;
+            this.LblPound.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblPound);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTotalPrice);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rdbPound);
+            this.Controls.Add(this.rdbEuro);
+            this.Controls.Add(this.rdbDollar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.lblDollar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDollar;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdbDollar;
+        private System.Windows.Forms.RadioButton rdbEuro;
+        private System.Windows.Forms.RadioButton rdbPound;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTotalPrice;
+        private System.Windows.Forms.Label LblPound;
+    }
+}
+
